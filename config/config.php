7467a316179194a32a8c575b26bb323d69553c47
stefanorosanelli <stefano.rosanelli@gmail.com>
2014-08-11 17:22:29 +0200

    code cleanup

--- config/config.php
+++ config/config.php
@@ -1,33 +1,9 @@
 <?php
-/*-----8<--------------------------------------------------------------------
- * 
- * BEdita - a semantic content management framework
- * 
- * Copyright 2010 ChannelWeb Srl, Chialab Srl
- * 
- * This file is part of BEdita: you can redistribute it and/or modify
- * it under the terms of the Affero GNU General Public License as published 
- * by the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version.
- * BEdita is distributed WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the Affero GNU General Public License for more details.
- * You should have received a copy of the Affero GNU General Public License 
- * version 3 along with BEdita (see LICENSE.AGPL).
- * If not, see <http://gnu.org/licenses/agpl-3.0.html>.
- * 
- *------------------------------------------------------------------->8-----
- */
 
 /**
- * Write here your module global configurations. This configurations will be loaded in every BEdita's page
+ * Write here your module global configurations. 
+ * This configurations will be loaded in every BEdita page
  * For example use it to define objects relations with other BEdita objects  
- *
- * @version			$Revision: 2590 $
- * @modifiedby 		$LastChangedBy: bato $
- * @lastmodified	$LastChangedDate: 2010-02-09 14:51:28 +0100 (Tue, 09 Feb 2010) $
- * 
- * $Id: config.php 2590 2010-02-09 13:51:28Z bato $
  */
 
 //$config["objRelationType"] = array(
